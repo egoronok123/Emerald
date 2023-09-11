a4e15bde10843fc5b67a98bf74a19710f3786fbf
Egor Dragel <metamorgasd@gmail.com>
2023-09-11 11:36:14 +0300

    update: update project

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TelegramForm from '../components/TelegramForm.tsx';
 
 const App: React.FC = () => {
+    useEffect(() => {
+        // Устанавливаем вертикальную прокрутку в начало страницы при загрузке
+        window.scrollTo(0, 0);
+    }, []);
     return (
-        <div className={'regContent   mt-28 '}>
+        <div className={'regContent   mt-28 '} >
             <TelegramForm />
         </div>
     );
